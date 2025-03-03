d0cfc5afc69081ab011b0be567c2ee807e9d6b78
Phil Austin <paustin@eos.ubc.ca>
2025-03-02 20:38:42 -0800

    update

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    print(f"{in inint {version('a301_lib')=}")
-    __version__ = version("a301_lib")
-except PackageNotFoundError:
-    # package is not installed
-    pass
